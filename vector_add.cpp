b18747b9ab1a09b099efac1197fe86db5511d0de
Diyu Hu <diyuhu@uvic.ca>
2022-04-03 16:02:21 -0700

    Added comments. Added exception handling. Used cl_float instead of built-in float.

--- vector_add.cpp
+++ vector_add.cpp
@@ -5,81 +5,96 @@
 #include <iostream>
 #include <vector>
 
-std::string kernel{R"(
-kernel void vector_add(global const float *a, global const float *b, global float *c) {
-    int i = get_global_id(0);
-    c[i] = a[i] + b[i];
-}
-)"};
-
 int main() {
+  try {
+    // Query available platforms.
     std::vector<cl::Platform> platforms;
     cl::Platform::get(&platforms);
 
-    for (const auto& platform : platforms) {
-        auto name = platform.getInfo<CL_PLATFORM_NAME>();
-        auto vendor = platform.getInfo<CL_PLATFORM_VENDOR>();
-        auto version = platform.getInfo<CL_PLATFORM_VERSION>();
-
-        std::cout << name << "\n";
-        std::cout << vendor << "\n";
-        std::cout << version << "\n";
+    for (const auto& platform: platforms) {
+      std::cout << "CL_PLATFORM_NAME: " << platform.getInfo<CL_PLATFORM_NAME>() << "\n";
+      std::cout << "CL_PLATFORM_VENDOR: " << platform.getInfo<CL_PLATFORM_VENDOR>() << "\n";
+      std::cout << "CL_PLATFORM_VERSION: " << platform.getInfo<CL_PLATFORM_VERSION>() << "\n";
     }
 
+    // Choose the first available platform.
     auto platform = platforms.front();
 
+    // Query available devices in platform.
     std::vector<cl::Device> devices;
     platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
 
-    for (const auto& device : devices) {
-        auto name = device.getInfo<CL_DEVICE_NAME>();
-        auto vendor = device.getInfo<CL_DEVICE_VENDOR>();
-        auto version = device.getInfo<CL_DEVICE_VERSION>();
-        auto profile = device.getInfo<CL_DEVICE_PROFILE>();
-
-        std::cout << name << "\n";
-        std::cout << vendor << "\n";
-        std::cout << version << "\n";
-        std::cout << profile << "\n";
+    for (const auto& device: devices) {
+      std::cout << "CL_DEVICE_NAME: " << device.getInfo<CL_DEVICE_NAME>() << "\n";
+      std::cout << "CL_DEVICE_VENDOR: " << device.getInfo<CL_DEVICE_VENDOR>() << "\n";
+      std::cout << "CL_DEVICE_VERSION: " << device.getInfo<CL_DEVICE_VERSION>() << "\n";
+      std::cout << "CL_DEVICE_PROFILE: " << device.getInfo<CL_DEVICE_PROFILE>() << "\n";
     }
 
+    // Choose the first available device in platform.
     auto device = devices.front();
 
+    // Create a context with device.
     cl::Context context{device};
-
+    // Create a command queue with context and device.
     cl::CommandQueue command_queue{context, device};
 
-    std::vector<std::string> program_strings{kernel};
+    // Use raw string store kernel source file.
+    std::string kernel_str{R"(
+kernel void vector_add(global const float *a, global const float *b, global float *c) {
+  int i = get_global_id(0);
+  c[i] = a[i] + b[i];
+}
+    )"};
 
+    // Create a program with kernel source.
+    std::vector<std::string> program_strings{kernel_str};
     cl::Program vector_add_program{context, program_strings};
+    // Try to build kernel.
     try {
-        vector_add_program.build("-cl-std=CL3.0");
+      vector_add_program.build("-cl-std=CL3.0");
     } catch (...) {
-        cl_int build_err = CL_SUCCESS;
-        auto build_info = vector_add_program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(&build_err);
-        for (auto &pair : build_info) {
-            std::cerr << pair.second << "\n";
-        }
-        return 1;
+      // Print build log if build failed (exception thrown).
+      cl_int build_err = CL_SUCCESS;
+      auto build_info = vector_add_program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(&build_err);
+      for (auto& pair: build_info) {
+        std::cerr << pair.second << "\n";
+      }
+      return 1;
     }
 
+    // Create host vectors.
     constexpr int vector_size = 1024 * 1024 * 64;
+    std::vector<cl_float> h_a(vector_size, 1.0f);
+    std::vector<cl_float> h_b(vector_size, 2.0f);
+    std::vector<cl_float> h_c(vector_size, 0.0f);
 
-    std::vector<float> h_a(vector_size, 1.0f);
-    std::vector<float> h_b(vector_size, 2.0f);
-    std::vector<float> h_c(vector_size, 0.0f);
-
+    // Create device buffers.
     cl::Buffer d_a{context, h_a.begin(), h_a.end(), true};
     cl::Buffer d_b{context, h_b.begin(), h_b.end(), true};
     cl::Buffer d_c{context, CL_MEM_WRITE_ONLY, sizeof(float) * vector_size};
 
-    cl::KernelFunctor<cl::Buffer, cl::Buffer, cl::Buffer> vector_add_kernel{vector_add_program, "vector_add"};
+    // Create vector add kernel object (represented by cl::KernelFunctor).
+    auto vector_add_kernel = cl::KernelFunctor<cl::Buffer, cl::Buffer, cl::Buffer>{vector_add_program, "vector_add"};
 
-    auto args = cl::EnqueueArgs(command_queue, cl::NDRange{1024});
+    // Start computation.
+    auto args = cl::EnqueueArgs(command_queue, cl::NDRange{1024}, cl::NDRange{64});
     vector_add_kernel(args, d_a, d_b, d_c);
+
+    // Copy result from device buffer to host vector.
     cl::copy(command_queue, d_c, h_c.begin(), h_c.end());
 
-    std::cout << h_c.front() << "\n";
+    // Check calculation results.
+    std::cout << "Result: " << h_c.front() << "\n";
+  } catch (const cl::Error& err) {
+    std::cerr << "CL ERROR: " << err.what() << "\n";
+    return 1;
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << "\n";
+    return 1;
+  } catch (...) {
+    throw;
+  }
 
-    return 0;
+  return 0;
 }
